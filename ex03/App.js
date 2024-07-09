60f6d1fd95e541fa3d8e72e0d845ef119def2898
leejw-lu <jiwoojenny@naver.com>
2024-07-09 17:13:46 +0900

    class: 모듈 분리 연습

---
+++ ex03/App.js
@@ -0,0 +1,7 @@
+/* App Module */
+
+module.exports = function() {
+    var app={};
+    app.textContents='hello world';
+    return app;
+}
---
+++ ex05/readme.txt
@@ -0,0 +1,4 @@
+Module programming IV : common.js(require) 기반
+
+1) math.js : exports 바로 사용
+2) index.js : math module 사용
